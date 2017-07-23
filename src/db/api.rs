f42372118ea7005cfbd4acdf43a38cd24f401cb9
Krishan Wyse <kwysek@gmail.com>
2017-07-23 14:55:50 +0100

    Fix failing test by hardcoding first post ID to 1

--- src/db/api.rs
+++ src/db/api.rs
@@ -12,16 +12,11 @@ pub fn find_post_by_id(post_id: i32, conn: DatabaseConnection) -> Option<BlogPos
 
 #[cfg(test)]
 mod tests {
-    use diesel::pg::PgConnection;
-    use r2d2::{Config, Pool};
-    use r2d2_diesel::ConnectionManager;
-    use std::env;
-
     use super::*;
 
     #[test]
     fn find_one_post_by_id_successfully() {
-        let conn = connection_with_published_blog_post();
+        let conn = connection_with_one_published_blog_post();
         let post = find_post_by_id(1, conn);
 
         assert!(post.is_some());
@@ -29,13 +24,18 @@ mod tests {
 
     #[test]
     fn fail_to_find_one_post_by_id() {
-        let conn = connection_with_published_blog_post();
-        let post = find_post_by_id(2, conn);
+        let conn = connection_with_no_blog_posts();
+        let post = find_post_by_id(1, conn);
 
         assert!(post.is_none());
     }
 
-    fn connection() -> DatabaseConnection {
+    fn connection_with_no_blog_posts() -> DatabaseConnection {
+        use diesel::pg::PgConnection;
+        use r2d2::{Config, Pool};
+        use r2d2_diesel::ConnectionManager;
+        use std::env;
+
         let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
         let config = Config::builder().pool_size(1).build();
         let manager = ConnectionManager::<PgConnection>::new(database_url);
@@ -46,13 +46,12 @@ mod tests {
         DatabaseConnection(conn)
     }
 
-    fn connection_with_published_blog_post() -> DatabaseConnection {
-        let conn = connection();
+    fn connection_with_one_published_blog_post() -> DatabaseConnection {
+        let conn = connection_with_no_blog_posts();
         conn.execute("
             INSERT INTO blog_posts
-            (title, content, published)
             VALUES
-            ('Post Title', 'The content of the post', true)
+            (1, 'Post Title', 'The content of the post', true)
         ").unwrap();
         conn
     }
