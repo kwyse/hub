7c3dcd5e4e1039dd4bb22a0bf49e2f65a165b67a
Krishan Wyse <kwysek@gmail.com>
2017-07-23 10:44:20 +0100

    Add authentication on /api route

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
 
-
 #[macro_use]
 extern crate diesel;
 #[macro_use]
 extern crate diesel_codegen;
+#[macro_use]
+extern crate rocket_contrib;
 #[macro_use]
 extern crate serde_derive;
 #[macro_use]
@@ -15,7 +16,6 @@ extern crate config;
 extern crate r2d2;
 extern crate r2d2_diesel;
 extern crate rocket;
-extern crate rocket_contrib;
 extern crate regex;
 extern crate serde;
 extern crate slog_async;
@@ -24,6 +24,8 @@ extern crate slog_term;
 use slog::{Drain, Logger};
 use slog_async::Async;
 use slog_term::{FullFormat, TermDecorator};
+
+use routes::api;
 
 mod db;
 mod routes;
@@ -36,11 +38,15 @@ fn main() {
         .fuse();
     let logger = &Logger::root(drain, o!());
 
-    let settings = &settings::load(logger);
-    let connection_pool = db::establish_pool(settings).unwrap();
+    let settings = settings::load(logger);
+    let connection_pool = db::establish_pool(&settings)
+        .expect("Failed to initialize database connection pool");
 
     rocket::ignite()
         .manage(connection_pool)
-        .mount("/api", routes::api::get())
+        .manage(settings)
+        .mount("/api", routes![
+            api::show
+        ])
         .launch();
 }
--- src/routes/api.rs
+++ src/routes/api.rs
@@ -1,22 +1,53 @@
 use diesel::prelude::*;
-use rocket::Route;
-use rocket::response::status;
-use rocket_contrib::Json;
+use rocket::{Request, State, Outcome};
+use rocket::http::Status;
+use rocket::request::{FromRequest, Outcome as RequestOutcome};
+use rocket_contrib::{Json, Value};
+use serde::Serialize;
 
 use db::DatabaseConnection;
 use db::models::Article;
 use db::schema::articles::dsl::*;
-
-pub fn get() -> Vec<Route> {
-    routes![
-        show
-    ]
-}
+use settings::Settings;
 
 #[get("/articles/<article_id>")]
-pub fn show(article_id: i32, conn: DatabaseConnection) -> Result<Json<Article>, status::NotFound<String>> {
-    articles.find(article_id)
-        .first::<Article>(&*conn)
-        .map(|article| Json(article))
-        .map_err(|_| status::NotFound(String::from("No article found for provided ID")))
+pub fn show(article_id: i32, conn: DatabaseConnection, _key: ApiKey) -> Json<Value> {
+    match articles.find(article_id).first::<Article>(&*conn) {
+        Ok(article) => json_as_success(article),
+        Err(_) => json_as_error("Article not found")
+    }
+}
+
+fn json_as_success<T: Serialize>(data: T) -> Json<Value> {
+    Json(json!({
+        "status": "success",
+        "data": data,
+    }))
+}
+
+fn json_as_error(reason: &str) -> Json<Value> {
+    Json(json!({
+        "status": "error",
+        "reason": reason,
+    }))
+}
+
+pub struct ApiKey(String);
+
+impl<'a, 'r> FromRequest<'a, 'r> for ApiKey {
+    type Error = ();
+
+    fn from_request(request: &'a Request<'r>) -> RequestOutcome<ApiKey, ()> {
+        let settings = request.guard::<State<Settings>>()?;
+        match request.headers().get_one("Authorization") {
+            Some(key) => {
+                if key == settings.api_key {
+                    Outcome::Success(ApiKey(key.to_string()))
+                } else {
+                    Outcome::Failure((Status::Unauthorized, ()))
+                }
+            },
+            None => Outcome::Failure((Status::Unauthorized, ()))
+        }
+    }
 }
